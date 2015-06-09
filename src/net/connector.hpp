976abb4591bc62c8ff4627092d7907c1972d47a3
ruoka <ruoka@users.noreply.github.com>
2015-06-09 22:55:08 +0300

    Update connector.hpp

--- src/net/connector.hpp
+++ src/net/connector.hpp
@@ -19,22 +19,22 @@ public:
 
     const std::string& host() const
     {
-		return m_host;
+        return m_host;
     }
 
     const std::string& service() const
     {
-		return m_service;
+        return m_service;
     }
 
     const std::chrono::milliseconds& timeout() const
     {
-		return m_timeout;
+        return m_timeout;
     }
 
     void timeout(const std::chrono::milliseconds& timeout)
     {
-		m_timeout = timeout;
+        m_timeout = timeout;
     }
 
 private:
