94621ddd0d36689aa09c6a8ed27255556b62ecd4
Kaius Ruokonen <karu@Kaiuss-MBP.Home>
2015-06-18 00:45:00 +0300

    Added some examples

---
+++ test/net/examples.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "net/connector.hpp"
+#include "net/acceptor.hpp"
+
+using namespace std;
+using namespace net;
+
+TEST(ExampleHttpRequest,commandLine)
+try
+{
+    iostream ios{connect("www.google.com","http")};
+
+    ios << "GET / HTTP/1.1\r\n"
+        << "Host: www.google.com\r\n"
+        << "Connection: close\r\n"
+        << "Accept: text/plain, text/html\r\n"
+        << "Accept-Charset: utf-8\r\n"
+        << "\r\n"
+        << flush;
+
+    while(ios)
+    {
+        char c;
+        ios >> noskipws >> c;
+        clog << c;
+    }
+    clog << flush;
+}
+catch(const exception& e)
+{
+    cerr << "Exception: " << e.what() << endl;
+}
+
+TEST(ExampleEchoServer,commandLine)
+try
+{
+    acceptor ator{"::1", "2112"}; // IPv6 localhost 
+    iostream ios{ator.accept()};
+
+    while(ios)
+    {
+        string echo;
+        getline(ios, echo);
+        ios << echo << endl;
+        clog << echo << endl;
+    }
+}
+catch(const exception& e)
+{
+    cerr << "Exception: " << e.what() << endl;
+}
+
