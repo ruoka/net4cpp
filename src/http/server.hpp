00788e9df556961c626c01f35d0b692db5956a61
--global <ruoka@users.noreply.github.com>
2016-09-04 23:48:43 +0300

    Fixed defects

--- src/http/server.hpp
+++ src/http/server.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <functional>
 #include <thread>
 #include <unordered_map>
@@ -7,6 +9,7 @@
 using namespace std;
 using namespace chrono;
 using namespace string_literals;
+using namespace net;
 
 namespace http {
 
@@ -44,6 +47,11 @@ namespace http {
             return m_router[path]["GET"s];
         }
 
+        controller& head(const string& path)
+        {
+            return m_router[path]["HEAD"s];
+        }
+
         controller& post(const string& path)
         {
             return m_router[path]["POST"s];
@@ -52,6 +60,11 @@ namespace http {
         controller& put(const string& path)
         {
             return m_router[path]["PUT"s];
+        }
+
+        controller& patch(const string& path)
+        {
+            return m_router[path]["PATCH"s];
         }
 
         controller& destroy(const string& path)
@@ -77,28 +90,34 @@ namespace http {
         {
             while(connection)
             {
-                auto method = ""s, path = ""s, line = "xxx"s;
-                connection >> method >> path;
+                auto method = ""s, uri = ""s, version = ""s;
+                connection >> method >> uri >> version;
 
-                clog << method << ' ' << path << ' ';
+                clog << method << ' ' << uri << ' ' << version;
 
-                while(connection && line.length() > 2)
+                connection >> ws;
+
+                while(connection && connection.peek() != '\r')
                 {
-                    getline(connection, line);
-                    clog << line << '\n';
+                    auto name = ""s, value = ""s;
+                    getline(connection, name, ':');
+                    trim(name);
+                    getline(connection, value);
+                    trim(value);
+                    clog << name << ": " << value << endl;
                 }
 
-                const auto content = m_router[path][method].render();
+                const auto content = m_router[uri][method].render();
 
-                connection << "HTTP/1.1 200 OK\r\n"
-                       << "Date: " << std::to_rfc1123(system_clock::now()) << "\r\n"
-                       << "Server: http://localhost:8080" << path << "\r\n" // FIXME
-                       << "Access-Control-Allow-Origin: *\r\n"
-                       << "Access-Control-Allow-Methods: POST, GET, PUT, DELETE\r\n"
-                       << "Content-Type: text/html; charset=UTF-8\r\n"
-                       << "Content-Length: " << content.length() << "\r\n"
-                       << "\r\n"
-                       << content << flush;
+                connection << "HTTP/1.1 200 OK"                                                   << crlf
+                           << "Date: " << to_rfc1123(system_clock::now())                         << crlf
+                           << "Server: YARESTDB/0.1"                                              << crlf
+                           << "Access-Control-Allow-Origin: *"                                    << crlf
+                           << "Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE" << crlf
+                           << "Content-Type: text/html; charset=UTF-8"                            << crlf
+                           << "Content-Length: " << content.length()                              << crlf
+                           << crlf
+                           << (method != "HEAD"s ? content : ""s) << flush;
             }
         }
 
--- test/http/server_test.cpp
+++ test/http/server_test.cpp
@@ -26,8 +26,10 @@ u8R"(
 <body>
     <aside>
         <button type="button" data-url="/vk" data-method="GET"   > GET</button>
+        <button type="button" data-url="/vk" data-method="HEAD"  > HEAD</button>
         <button type="button" data-url="/vk" data-method="POST"  > POST</button>
         <button type="button" data-url="/vk" data-method="PUT"   > PUT</button>
+        <button type="button" data-url="/vk" data-method="PATCH" > PATCH</button>
         <button type="button" data-url="/vk" data-method="DELETE"> DELETE</button>
         <button type="button" data-url="/json" data-method="GET" > JSON</button>
     </aside>
@@ -39,11 +41,15 @@ u8R"(
 
 const auto get = u8R"(<p>GET response</p>)"s;
 
+const auto head = u8R"(<p>HEAD response</p>)"s;
+
 const auto post = u8R"(<p>POST response</p>)"s;
 
 const auto put = u8R"(<p>PUT response</p>)"s;
 
 const auto destroy = u8R"(<p>DELETE response</p>)"s;
+
+const auto patch = u8R"(<p>PATCH response</p>)"s;
 
 const auto json = u8R"( {"foo" : 1, "bar" : false} )"s;
 
@@ -55,8 +61,10 @@ TEST(HttpServer,Setup)
 
     server.get("/"s).response(view::index);
     server.get("/vk"s).response(view::get);
+    server.head("/vk"s).response(view::head);
     server.post("/vk"s).response(view::post);
     server.put("/vk"s).response(view::put);
+    server.patch("/vk"s).response(view::patch);
     server.destroy("/vk"s).response(view::destroy);
     server.get("/json"s).response(view::json);
 
