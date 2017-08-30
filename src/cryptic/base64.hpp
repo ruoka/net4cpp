fd8acb64b4ed946581797f6ece3b6d9c90ed94ff
ruoka <ruoka@users.noreply.github.com>
2017-08-30 21:09:24 +0300

    Cleanup

--- src/cryptic/base64.hpp
+++ src/cryptic/base64.hpp
@@ -63,11 +63,6 @@ namespace cryptic::base64 {
         return encoded;
     }
 
-    // inline std::string encode(std::string_view source)
-    // {
-    //     return encode(source);
-    // }
-
     inline std::string decode(std::string_view source)
     {
         auto decoded = std::string{};
