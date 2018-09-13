3f4eabf061634bda3baaa44819eda443323a06e7
ruoka <ruoka@users.noreply.github.com>
2018-09-13 22:16:20 +0300

    Fixes

--- src/gsl/span.hpp
+++ src/gsl/span.hpp
@@ -1,13 +1,13 @@
 #pragma once
 #include <iterator>
 #include <algorithm>
-#include <experimental/type_traits>
+#include <type_traits>
 #include "gsl/assert.hpp"
 
 namespace gsl {
 
 // [views.constants], constants
-constexpr std::ptrdiff_t dynamic_extent = -1;
+constexpr std::ptrdiff_t dynamic_extent = std::numeric_limits<std::ptrdiff_t>::max();
 
 template<std::ptrdiff_t Extent>
 struct __span_size
@@ -64,33 +64,49 @@ public:
     static constexpr index_type extent = Extent;
 
     // [span.cons], span constructors, copy, assignment, and destructor constexpr span();
-    constexpr span(std::nullptr_t) : m_data{nullptr}, m_size{0}
+    constexpr span(std::nullptr_t) :
+        m_data{nullptr},
+        m_size{0}
     {}
 
-    constexpr span(pointer ptr, index_type count) : m_data{ptr}, m_size{count}
+    constexpr span(pointer ptr, index_type count) :
+        m_data{ptr},
+        m_size{count}
     {}
 
-    constexpr span(pointer firstElem, pointer lastElem) : m_data{firstElem}, m_size{std::distance(firstElem,lastElem)}
+    constexpr span(pointer firstElem, pointer lastElem) :
+        m_data{firstElem},
+        m_size{std::distance(firstElem,lastElem)}
     {}
 
     template <size_t N>
-    constexpr span(element_type (&arr)[N]) : m_data{arr}, m_size{N}
+    constexpr span(element_type (&arr)[N]) :
+        m_data{arr},
+        m_size{N}
     {}
 
     template <size_t N>
-    constexpr span(std::array<std::remove_const_t<element_type>, N>& arr) : m_data{arr.data()}, m_size{N}
+    constexpr span(std::array<std::remove_const_t<element_type>, N>& arr) :
+        m_data{arr.data()},
+        m_size{N}
     {}
 
     template <size_t N>
-    constexpr span(const std::array<std::remove_const_t<element_type>, N>& arr) : m_data{arr.data()}, m_size{N}
+    constexpr span(const std::array<std::remove_const_t<element_type>, N>& arr) :
+        m_data{const_cast<element_type*>(arr.data())},
+        m_size{N}
     {}
 
     template <class Container>
-    constexpr span(Container& cont) : m_data{reinterpret_cast<ElementType*>(cont.data())}, m_size{cont.size()}
+    constexpr span(Container& cont) :
+        m_data{reinterpret_cast<element_type*>(cont.data())},
+        m_size{cont.size()}
     {}
 
     template <class Container>
-    constexpr span(const Container& cont) : m_data{reinterpret_cast<const ElementType*>(cont.data())}, m_size{cont.size()}
+    constexpr span(const Container& cont) :
+        m_data{const_cast<element_type*>(reinterpret_cast<const element_type*>(cont.data()))},
+        m_size{cont.size()}
     {}
 
     // template <class Container> span(const Container&&) = delete;
@@ -99,18 +115,18 @@ public:
 
     constexpr span(span&& other) noexcept = default;
 
-    template <class OtherElementType, ptrdiff_t OtherExtent>
-    constexpr span(const span<OtherElementType, OtherExtent>& other) : span<ElementType,Extent>{other.data(), other.size()}
+    template <class OtherElementType, std::ptrdiff_t OtherExtent>
+    constexpr span(const span<OtherElementType, OtherExtent>& other) :
+        span{other.data(), other.size()}
     {
-        using std::experimental::is_convertible_v;
         static_assert(std::is_convertible_v<OtherElementType,ElementType>, "Not convertible");
         static_assert(OtherExtent <= Extent, "Size mismatch");
     }
 
-    template <class OtherElementType, ptrdiff_t OtherExtent>
-    constexpr span(span<OtherElementType, OtherExtent>&& other) : span<ElementType,Extent>{other.data(), other.size()}
+    template <class OtherElementType, std::ptrdiff_t OtherExtent>
+    constexpr span(span<OtherElementType, OtherExtent>&& other) :
+        span{other.data(), other.size()}
     {
-        using std::experimental::is_convertible_v;
         static_assert(std::is_convertible_v<OtherElementType,ElementType>, "Not convertible");
         static_assert(OtherExtent <= Extent, "Size mismatch");
     }
@@ -122,19 +138,21 @@ public:
     constexpr span& operator=(span&& other) noexcept = default;
 
     // [span.sub], span subviews
-    template <ptrdiff_t Count>
+    template <std::ptrdiff_t Count>
     constexpr span<element_type, Count> first() const
     {
-        return first(Count);
+        Expects(Count <= m_size);
+        return {m_data,Count};
     }
 
-    template <ptrdiff_t Count>
+    template <std::ptrdiff_t Count>
     constexpr span<element_type, Count> last() const
     {
-        return last(Count);
+        Expects(Count <= m_size);
+        return {m_data + m_size - Count, Count};
     }
 
-    template <ptrdiff_t Offset, ptrdiff_t Count = dynamic_extent>
+    template <std::ptrdiff_t Offset, std::ptrdiff_t Count = dynamic_extent>
     constexpr span<element_type, Count> subspan() const
     {
         return subspan(Offset, Count);
@@ -142,19 +160,19 @@ public:
 
     constexpr span<element_type, dynamic_extent> first(index_type count) const
     {
-        Ensures(count <= m_size);
+        Expects(count <= m_size);
         return {m_data,count};
     }
 
     constexpr span<element_type, dynamic_extent> last(index_type count) const
     {
-        Ensures(count <= m_size);
+        Expects(count <= m_size);
         return {m_data + m_size - count, count};
     }
 
     constexpr span<element_type, dynamic_extent> subspan(index_type offset, index_type count = dynamic_extent) const
     {
-        Ensures(offset <= m_size);
+        Expects(offset <= m_size);
         return {m_data + offset, (count == dynamic_extent) ? (m_size - offset) : count};
     }
 
@@ -187,13 +205,13 @@ public:
     // [span.elem], span element access
     constexpr reference operator[](index_type idx) const
     {
-        Ensures(idx < m_size);
+        Expects(idx < m_size);
         return m_data[idx];
     }
 
     constexpr reference operator()(index_type idx) const
     {
-        Ensures(idx < m_size);
+        Expects(idx < m_size);
         return m_data[idx];
     }
 
@@ -253,53 +271,53 @@ private:
 };
 
 // [span.comparison], span comparison operators
-template <class ElementType, ptrdiff_t Extent>
+template <class ElementType, std::ptrdiff_t Extent>
 constexpr bool operator==(const span<ElementType, Extent>& l, const span<ElementType, Extent>& r) noexcept
 {
     return std::equal(l.cbegin(),l.cend(),r.cbegin(),r.cend());
 }
 
-template <class ElementType, ptrdiff_t Extent>
+template <class ElementType, std::ptrdiff_t Extent>
 constexpr bool operator!=(const span<ElementType, Extent>& l, const span<ElementType, Extent>& r) noexcept
 {
     return !(l == r);
 }
 
-template <class ElementType, ptrdiff_t Extent>
+template <class ElementType, std::ptrdiff_t Extent>
 constexpr bool operator<(const span<ElementType, Extent>& l, const span<ElementType, Extent>& r) noexcept
 {
     return std::lexicographical_compare(l.cbegin(),l.cend(),r.cbegin(),r.cend());
 }
 
-template <class ElementType, ptrdiff_t Extent>
+template <class ElementType, std::ptrdiff_t Extent>
 constexpr bool operator<=(const span<ElementType, Extent>& l, const span<ElementType, Extent>& r) noexcept
 {
     return r == l || r < l;
 }
 
-template <class ElementType, ptrdiff_t Extent>
+template <class ElementType, std::ptrdiff_t Extent>
 constexpr bool operator>(const span<ElementType, Extent>& l, const span<ElementType, Extent>& r) noexcept
 {
     return std::lexicographical_compare(r.cbegin(),r.cend(),l.cbegin(),l.cend());
 }
 
-template <class ElementType, ptrdiff_t Extent>
+template <class ElementType, std::ptrdiff_t Extent>
 constexpr bool operator>=(const span<ElementType, Extent>& l, const span<ElementType, Extent>& r) noexcept
 {
     return r == l || r > l;
 }
 
 // [span.objectrep], views of object representation
-template <class ElementType, ptrdiff_t Extent>
-constexpr span<const std::byte, (Extent == dynamic_extent ? dynamic_extent : (sizeof(ElementType) * Extent))> as_bytes(span<ElementType, Extent> s) noexcept
+template <class ElementType, std::ptrdiff_t Extent>
+constexpr span<const std::byte, ((Extent == dynamic_extent) ? dynamic_extent : (Extent * sizeof(ElementType)))> as_bytes(span<ElementType, Extent> s) noexcept
 {
-    return {static_cast<const std::byte*>(s.data()), s.length_bytes()};
+    return {reinterpret_cast<const std::byte*>(s.data()), s.length_bytes()};
 }
 
-template <class ElementType, ptrdiff_t Extent>
-constexpr span<std::byte, (Extent == dynamic_extent ? dynamic_extent : (sizeof(ElementType) * Extent))> as_writeable_bytes(span<ElementType, Extent> s) noexcept
+template <class ElementType, std::ptrdiff_t Extent>
+constexpr span<std::byte, ((Extent == dynamic_extent) ? dynamic_extent : (Extent * sizeof(ElementType)))> as_writeable_bytes(span<ElementType, Extent> s) noexcept
 {
-    return {static_cast<std::byte*>(s.data()), s.length_bytes()};
+    return {reinterpret_cast<std::byte*>(s.data()), s.length_bytes()};
 }
 
 //
