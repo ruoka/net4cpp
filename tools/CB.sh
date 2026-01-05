bd983cfbbd4d72e1dcb7c661eb56a9e2f28fefb0
--global <ruoka@users.noreply.github.com>
2026-01-05 17:55:06 +0200

    fix: update CB.sh to use Clang 21 for Linux builds

--- tools/CB.sh
+++ tools/CB.sh
@@ -53,9 +53,9 @@ SRC="$TESTER_ROOT/tools/cb.c++"
 UNAME_OUT="$(uname -s)"
 case "$UNAME_OUT" in
     Linux)
-    CXX_COMPILER="clang++-20"
-    LLVM_PREFIX="/usr/lib/llvm-20"
-        STD_CPPM_DEFAULT="/usr/lib/llvm-20/share/libc++/v1/std.cppm"
+    CXX_COMPILER="clang++-21"
+    LLVM_PREFIX="/usr/lib/llvm-21"
+        STD_CPPM_DEFAULT="/usr/lib/llvm-21/share/libc++/v1/std.cppm"
         ;;
     Darwin)
     CXX_COMPILER="/usr/local/llvm/bin/clang++"
